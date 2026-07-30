1474b421e8dc672f228a12b29134bcfbfbd8bbff
SimusDev <simusdev@mail.ru>
2026-07-30 14:41:25 +0300

    Property Synchronizer WIP

---
+++ Scenes/World.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+namespace Scenes.World;
+public partial class World : Node3D
+{
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+
+
+}
