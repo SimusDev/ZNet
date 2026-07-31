9677c22f6576a027ae3779f36b97a87c3941df59
SimusDev <simusdev@mail.ru>
2026-07-31 19:41:09 +0300

    12321321

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -3,18 +3,18 @@ using System;
 
 namespace ZNet.Scenes
 {
-    public partial class MainMenu : Control
-    {
-        public override void _Ready()
-        {
-            ZNetMultiplayer.Instance.NetworkStatusChanged += OnNetworkStatusChanged;
-        }
-        private void OnNetworkStatusChanged(ZNetMultiplayer.NetworkStatus status)
-        {
-            if (status == ZNetMultiplayer.NetworkStatus.Ready)
-            {
-                Startup.LoadOrReloadGame();
-            }
-        }
-    }
+	public partial class MainMenu : Control
+	{
+		public override void _Ready()
+		{
+			ZNetMultiplayer.Instance.NetworkStatusChanged += OnNetworkStatusChanged;
+		}
+		private void OnNetworkStatusChanged(ZNetMultiplayer.NetworkStatus status)
+		{
+			if (status == ZNetMultiplayer.NetworkStatus.Ready)
+			{
+				Startup.LoadOrReloadGame();
+			}
+		}
+	}
 }
